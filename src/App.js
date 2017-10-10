620f3c0f9f6044b7501c8e59a714e0298e67df81
Thomas Nguyen <V7XH3@regions.com>
2017-10-10 09:01:48 -0500

    added add list function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
+  state = {
+    myList: []
+  }
+
+  add = ()=> {
+    this.setState({
+      myList: [...this.state.myList, 'eggs']
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <ul>
+          {
+            this.state.myList.map(
+              (val, index)=> <li key={index}>{val}</li>
+            )
+          }
+        </ul>
+        <button onClick={this.add}>Click</button>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
+
+
+/* A LIST EXAMPLE
+  const myList = ['one', 'two', 'three']
+  const ListItem = (props)=> <li>{props.text}</li>
+  export default ()=> (
+    <ul>
+      {
+        myList.map(
+          (val)=> <ListItem text={val}/>
+        )
+      }
+    </ul>
+  )
+*/
