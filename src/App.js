d1db44cdc047a549a443339a02f736c62561f183
Daniel Stahl <B0GX3@regions.com>
2017-10-12 08:02:48 -0500

    first lesson

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state={
+    myList:[]
+  }
+  spoof=()=>{
+    
+    this.setState({
+      myList:[...this.state.myList, "eggs"]
+    })
+  }
+  deleteItem=(value)=>{
+    this.setState({
+      myList:this.state.myList.filter(val=>value!==val)
+    })
+  }
   render() {
+    console.log(this.state)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <ul>
+          {this.state.myList.map((val, index)=><li key={index}>{val}</li>)}
+        </ul>
+        <button onClick={this.spoof}>Click me</button>
       </div>
     );
   }
 }
 
-export default App;
+export default App
+
