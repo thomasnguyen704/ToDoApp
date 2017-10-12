9305a46a8b5473c69a74e1d1a74cbc9316fd2353
Daniel Stahl <B0GX3@regions.com>
2017-10-12 08:58:06 -0500

    updated app to include material ui

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Button from 'material-ui/Button'
+import List, { ListItem, ListItemText } from 'material-ui/List';
+import Checkbox from 'material-ui/Checkbox'
 class App extends Component {
   state={
-    myList:[
-
-    ]
+    myList:[]
   }
-
+  onListItemClick=(index)=>()=>{
+    this.setState({
+      myList:this.state.myList.map((val, listIndex)=>{
+        if(index===listIndex){
+          return {...val, isChecked:!val.isChecked}
+        }
+        else{
+          return val
+        }
+      })
+    })
+  }
   onClick=()=>{
     this.setState({
-      myList:[...this.state.myList, "eggs"]
-      }
-    )
+      myList:[...this.state.myList, {
+        primaryText:"eggs",
+        onClick:this.onListItemClick,
+        isChecked:false
+      }]
+    })
   }
   render() {
     console.log(this.state)
     return (
       <div>
-        <ul>
-          {this.state.myList.map(val=><li>{val}</li>)}
-        </ul>
-        <button onClick={this.onClick}>Click Me</button>
+        <List>
+          {this.state.myList.map((val, index)=>(
+            <ListItem key={index} onClick={val.onClick(index)}>
+              <Checkbox checked={val.isChecked}/>
+              <ListItemText primary={val.primaryText}/>
+            </ListItem>
+          ))}
+        </List>
+        <Button raised color="primary" onClick={this.onClick}>Click Me</Button>
 
       </div>
     );
