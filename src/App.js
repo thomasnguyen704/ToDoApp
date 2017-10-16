d3f8122b4bc2d18723eb8d0b3b7fdd4fb36b55a5
Thomas Nguyen <thomas.nguyen@regions.com>
2017-10-16 17:19:39 -0500

    code blocks

--- src/App.js
+++ src/App.js
@@ -6,53 +6,50 @@ import List, { ListItem, ListItemText } from 'material-ui/List'
 import Checkbox from 'material-ui/Checkbox'
 
 class App extends Component{
-  state={
-    myList: []
-  }
-  
-  onListItemClick=(index)=>()=>{
-    this.setState({
-      myList:this.state.myList.map((val, listIndex)=>{
-        if(index===listIndex){
-          return{
-            ...val, 
-            isChecked: !val.isChecked
-          }
-        }
-        else{
-          return val
-        }
-      })
-    })
-  }
-  
-  onClick=()=>{
-    this.setState({
-      myList: [...this.state.myList{
-        primaryText: "eggs",
-        onClick: this.onListItemClick,
-        isChecked: false
-      }]
-    })
-  }
+	state={ myList: [] }
 
-  render() {
-    return (
-      <div>
-        <List>
-          {
-            this.state.myList.map((val, index)=>(
-              <ListItem key={index} onClick={val.onClick(index)}>
-                <Checkbox checked={val.isChecked}/>
-                <ListItemText primary={val.primaryText}/>
-              </ListItem>
-            ))
-          }
-        </List>
-        <Button raised color="primary" onClick={this.onClick}>Click Me</Button>
-      </div>
-    )
-  }
+	onListItemClick=(index)=>()=>{
+		this.setState({
+			myList:this.state.myList.map((val, listIndex)=>{
+				if(index===listIndex){
+					return{
+						...val, 
+						isChecked: !val.isChecked
+					}
+				} else {
+					return val
+				}
+			})
+		})
+	}
+
+	onClick=()=>{
+		this.setState({
+			myList: [...this.state.myList{
+				primaryText: "eggs",
+				onClick: this.onListItemClick,
+				isChecked: false
+			}]
+		})
+	}
+
+	render() {
+		return (
+			<div>
+				<List>
+					{
+						this.state.myList.map((val, index)=>(
+							<ListItem key={index} onClick={val.onClick(index)}>
+							<Checkbox checked={val.isChecked}/>
+							<ListItemText primary={val.primaryText}/>
+							</ListItem>
+						))
+					}
+				</List>
+				<Button raised color="primary" onClick={this.onClick}>Click Me</Button>
+			</div>
+		)
+	}
 }
 
 export default App
